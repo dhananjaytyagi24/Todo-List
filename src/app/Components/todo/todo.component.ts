06ec085c4d75b6e73fb1278b22acb46545beaa31
Dhananjay Tyagi <dhananjaytyagi24@gmail.com>
2023-07-18 22:44:27 -0700

    addded todo components

---
+++ src/app/Components/todo/todo.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Todo } from 'src/app/TodoModel';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent {
+  todos: Todo[];
+
+  constructor(){
+    this.todos = [
+      {
+        'id' : 1,
+        'description' : 'Description',
+        'title' : 'Title',
+        'isActive': true
+      },
+      {
+        'id' : 2,
+        'description' : 'Description2',
+        'title' : 'Title2',
+        'isActive': true
+      },
+      {
+        'id' : 3,
+        'description' : 'Description3',
+        'title' : 'Title3',
+        'isActive': true
+      }
+    ];
+  }
+}
